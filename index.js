5cd9872b900b2b2c984e1db0358684104c53cc4b
Chris Athanas <realityexpander@gmail.com>
2019-05-17 02:24:23 -0500

    got forecast working

--- index.js
+++ index.js
@@ -67,8 +67,88 @@ async function getGrossSales(nodeId, date) {
 
 
 async function getForecastVariance(nodeId, date) {
-    // TODO:
-    return {};
+    let storeHierarchy = await getStoreHierarchy();
+    let storeForecasts = await getStoreSalesForecasts();
+    let storeSales = await getStoreSales();
+    let result = { total: 0 };
+    let grandTotalSales = 0;
+    let grandTotalForecast = 0;
+
+    // Get the region
+    for (let store of storeHierarchy.children) {
+        if (store.id === nodeId) {
+            [result, ,] = processStoreForecastChildren(store, storeForecasts, storeSales, date, {});
+            result.name = store.name;
+            result.totalSales = grandTotalSales;
+            result.totalForecast = grandTotalForecast;
+            result.variance = (grandTotalSales / grandTotalForecast) / 100;
+            break;
+        }
+    }
+
+    return result;
+
+    function processStoreForecastChildren(storeHeirarchy, storeForecasts, storeSales, date, result) {
+        let subTotalSales = 0;
+        let subTotalForecast = 0;
+
+        // Get the totals for individual stores
+        for (let theCurrentStore of storeHeirarchy.children) {
+            if (theCurrentStore.isStore) {
+                // Add up all gross forecast for the current store on date
+                let currentStoreForecastSubtotal = 0;
+                let currentStoreSalesSubtotal = 0;
+                for (let storeForecast of storeForecasts) {
+                    if (storeForecast.storeId === theCurrentStore.id &&
+                        storeForecast.businessDay === date) {
+                        currentStoreForecastSubtotal += storeForecast.forecastGrossSales;
+                    }
+                }
+
+                // Add up all gross sales for the current store on date
+                for (let storeSale of storeSales) {
+                    if (storeSale.storeId === theCurrentStore.id &&
+                        storeSale.businessDay === date) {
+                        currentStoreSalesSubtotal += storeSale.grossSales;
+                    }
+                }
+
+
+                // create child node for results
+                if (!result.children) result.children = [];
+                result.children.push({
+                    name: theCurrentStore.name,
+                    totalSales: currentStoreSalesSubtotal,
+                    totalForecast: currentStoreForecastSubtotal,
+                    variance: (currentStoreSalesSubtotal / currentStoreForecastSubtotal) / 100
+                });
+
+                // compute the totals for the stores in this zone
+                result.totalSales = result.totalSales ? result.totalSales + currentStoreSalesSubtotal : currentStoreSalesSubtotal;
+                result.totalForecast = result.totalForecast ? result.totalForecast + currentStoreForecastSubtotal : currentStoreForecastSubtotal;
+                subTotalSales = result.totalSales;
+                subTotalForecast = result.totalForecast;
+                grandTotalSales += currentStoreSalesSubtotal;
+                grandTotalForecast += currentStoreForecastSubtotal;
+            } else {
+                // create output nodes & recurse
+                if (!result.children) {
+                    result.children = [];
+                    result.totalSales = 0;
+                    result.totalForecast = 0;
+                }
+                result.children.push({ name: theCurrentStore.name });
+
+                [, subTotalSales, subTotalForecast] = processStoreForecastChildren(theCurrentStore, storeForecast, storeSales, date, result.children[result.children.length - 1]);
+                result.totalSales += subTotalSales;
+                result.totalForecast += subTotalForecast;
+                result.variance = (result.totalSales / result.totalForecast) / 100;
+            }
+        }
+
+        return [result, subTotalSales, subTotalForecast];
+
+    }
 }
 
 
@@ -82,19 +162,19 @@ async function getForecastVariance(nodeId, date) {
 // getForecastVariance node 12 OK = true
 // **************************************
 
-getGrossSales(11, '2018-07-31').then(grossSales => {
-    ;
-    console.log("getGrossSales node 11 OK = " + _.isEqual(grossSales, grossSalesSnapshot11));
-});
-
-getGrossSales(12, '2018-08-01').then(grossSales => {
-    ;
-    console.log("getGrossSales node 12 OK = " + _.isEqual(grossSales, grossSalesSnapshot12));
-});
-
-// getForecastVariance(11, '2018-07-31').then(forecastVariance => {
-//     console.log("getForecastVariance node 11 OK = " + _.isEqual(forecastVariance, forecastVarianceSnapshot11));
+// getGrossSales(11, '2018-07-31').then(grossSales => {
+//     ;
+//     console.log("getGrossSales node 11 OK = " + _.isEqual(grossSales, grossSalesSnapshot11));
 // });
+
+// getGrossSales(12, '2018-08-01').then(grossSales => {
+//     ;
+//     console.log("getGrossSales node 12 OK = " + _.isEqual(grossSales, grossSalesSnapshot12));
+// });
+
+getForecastVariance(11, '2018-07-31').then(forecastVariance => {
+    console.log("getForecastVariance node 11 OK = " + _.isEqual(forecastVariance, forecastVarianceSnapshot11));
+});
 
 // getForecastVariance(12, '2018-08-01').then(forecastVariance => {
 //     console.log("getForecastVariance node 12 OK = " + _.isEqual(forecastVariance, forecastVarianceSnapshot12));
